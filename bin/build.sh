21d8e26753f92610b1308f5625a7a57877d6f77d
Sandeep N <sandeepnm7@gmail.com>
2024-01-28 14:18:14 +0530

    Error Fixes and Indentation

---
+++ bin/build.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# XDEBUG_MODE=${XDEBUG_MODE:-off} docker compose -f docker-compose.yaml -f \
+#   docker-compose.dev.yaml --env-file .env up "$@"
+
+docker compose -f docker-compose.yaml --env-file .env up
