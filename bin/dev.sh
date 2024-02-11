8dee82214e64f829b1170bd47497ee6d646c1eb5
Sandeep N <sandeepnm7@gmail.com>
2024-02-12 00:16:32 +0530

    Error Resolutions

--- bin/dev.sh
+++ bin/dev.sh
@@ -3,4 +3,4 @@
 # XDEBUG_MODE=${XDEBUG_MODE:-off} docker compose -f docker-compose.yaml -f \
 #   docker-compose.dev.yaml --env-file .env up "$@"
 
-XDEBUG_MODE=${XDEBUG_MODE:-on} docker compose -f docker-compose.dev.yaml --env-file .env.local up --build
+XDEBUG_MODE=${XDEBUG_MODE:-off} docker compose -f docker-compose.dev.yaml --env-file .env.dev up --build -d
